e39f046e87c7a7e03c70afa7add6fe91531d2795
Alexandre Voiney <1880968+avoiney@users.noreply.github.com>
2024-09-04 11:46:49 +0200

    mer. 04 sept. 2024 11:46:49 CEST

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,9 +4,18 @@ return {
   { "Yazeed1s/oh-lucy.nvim" },
   { "EdenEast/nightfox.nvim" },
   {
+    "uloco/bluloco.nvim",
+    lazy = false,
+    priority = 1000,
+    dependencies = { "rktjmp/lush.nvim" },
+    config = function()
+      -- your optional config goes here, see below.
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nightfox",
+      colorscheme = "bluloco-light",
     },
   },
 }
